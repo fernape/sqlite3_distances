761e1064094ef51dda78c5a6c2505ee2450fde18
Fernando <fernando.apesteguia@gmail.com>
2019-08-14 17:23:26 +0200

    Remove copied comments
    They come from the template in sqlite3's documentation

--- hamming/hamming.c
+++ hamming/hamming.c
@@ -50,24 +50,12 @@ hamming_distance(sqlite3_context *context, int argc, sqlite3_value **argv)
 #ifdef _WIN32
 __declspec(dllexport)
 #endif
-/* TODO: Change the entry point name so that "extension" is replaced by
- ** text derived from the shared library filename as follows:  Copy every
- ** ASCII alphabetic character from the filename after the last "/" through
- ** the next following ".", converting each character to lowercase, and
- ** discarding the first three characters if they are "lib".
- */
+
 int sqlite3_hamming_init(sqlite3 *db, char **pzErrMsg
 		, const sqlite3_api_routines *pApi)
 {
 	int rc = SQLITE_OK;
 	SQLITE_EXTENSION_INIT2(pApi);
-	/* Insert here calls to
-	 **     sqlite3_create_function_v2(),
-	 **     sqlite3_create_collation_v2(),
-	 **     sqlite3_create_module_v2(), and/or
-	 **     sqlite3_vfs_register()
-	 ** to register the new features that your extension adds.
-	 */
 
 	rc = sqlite3_create_function(db, "hamming", 2,
 			SQLITE_UTF8|SQLITE_DETERMINISTIC, NULL,
