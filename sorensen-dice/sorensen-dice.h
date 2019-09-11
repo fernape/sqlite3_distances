d09e9b30455b06e90b2d0f64290da9f556d32d6f
Fernando <fernando.apesteguia@gmail.com>
2019-09-11 17:58:44 +0200

    sorensen-dice: add fuzzer

---
+++ sorensen-dice/sorensen-dice.h
@@ -0,0 +1,6 @@
+#ifndef __SORENSEN_DICE_DISTANCE__
+#define __SORENSEN_DICE_DISTANCE__
+
+double _sorensen_dice_distance(const char *str_a, const char *str_b);
+
+#endif /* __SORENSEN_DICE_DISTANCE__ */
---
+++ sorensen-dice/sorensen_dice_fuzzer.c
@@ -0,0 +1,29 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include "sorensen-dice.h"
+
+// fuzz_target.cc
+int
+LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size)
+{
+	char *str_a = malloc(Size);
+	char *str_b = malloc(Size);
+
+	if (str_a == NULL || str_b == NULL || Size == 0) {
+		return 0;
+	}
+
+	int len = Size - 1;
+	strncpy(str_a, (const char *)Data, len);
+	strncpy(str_b, (const char *)Data, len);
+	str_a[len] = '\0';
+	str_b[len] = '\0';
+
+	_sorensen_dice_distance(str_a, str_b);
+
+	free(str_a);
+	free(str_b);
+
+	return 0;
+}
