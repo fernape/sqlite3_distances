94478a6b1471d70d519992cc374aea1fab8de9fa
Fernando <fernando.apesteguia@gmail.com>
2019-12-14 21:03:14 +0100

    Add optimal string alignment distance.
    Also called Edit distance

---
+++ optimal_string_alignment/optimal_string_alignment.h
@@ -0,0 +1,6 @@
+#ifndef __OPTIMAL_STRING_ALIGNMENT_DISTANCE__
+#define __OPTIMAL_STRING_ALIGNMENT_DISTANCE__
+
+int _optimal_string_alignment_distance(const char *str_a, const char *str_b);
+
+#endif /* __OPTIMAL_STRING_ALIGNMENT_DISTANCE__ */
