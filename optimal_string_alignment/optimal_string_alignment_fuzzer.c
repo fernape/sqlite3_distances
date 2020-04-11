cd314f3d224ec1761344e4912c1fb30b8bf942bd
Fernando <fernando.apesteguia@gmail.com>
2020-04-11 20:43:26 +0200

    Improve fuzzer randomness.
    Create two strings of different random size never greater than "Size".
    
    TODO: Refactor *_fuzzer.c since we're doing the same thing in all of them.

--- optimal_string_alignment/optimal_string_alignment_fuzzer.c
+++ optimal_string_alignment/optimal_string_alignment_fuzzer.c
@@ -12,18 +12,24 @@
 int
 LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size)
 {
-	char *str_a = malloc(Size);
-	char *str_b = malloc(Size);
+	sranddev();
+
+	const int a_size = (int)(random() % (Size + 1));
+	const int b_size = (int)(random() % (Size + 1));
+
+	char *str_a = malloc(a_size);
+	char *str_b = malloc(b_size);
 
 	if (str_a == NULL || str_b == NULL || Size == 0) {
 		return 0;
 	}
 
-	int len = Size - 1;
-	strncpy(str_a, (const char *)Data, len);
-	strncpy(str_b, (const char *)Data, len);
-	str_a[len] = '\0';
-	str_b[len] = '\0';
+	int a_len = a_size > 0 ? a_size - 1 : 0;
+	int b_len = b_size > 0 ? b_size - 1 : 0;
+	strncpy(str_a, (const char *)Data, a_len);
+	strncpy(str_b, (const char *)Data, b_len);
+	str_a[a_len] = '\0';
+	str_b[b_len] = '\0';
 
 	_optimal_string_alignment_distance(str_a, str_b);
 
