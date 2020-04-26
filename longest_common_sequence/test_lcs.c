0dacdb74288e74f9d7d05761d7cf62bcc2461391
Fernando <fernando.apesteguia@gmail.com>
2020-04-26 19:39:59 +0200

    Add tests
    Add a bunch of tests. All of them pass.
    
    Fuzzer: runs without errors

--- longest_common_sequence/test_lcs.c
+++ longest_common_sequence/test_lcs.c
@@ -61,6 +61,192 @@ START_TEST(test_both_empty)
 }
 END_TEST
 
+START_TEST(test_lcs_is_0)
+{
+	ck_assert_int_eq(_lcs_distance("a", "b"), 0);
+}
+END_TEST
+
+START_TEST(test_lcs_is_1_a)
+{
+	ck_assert_int_eq(_lcs_distance("a", "ba"), 1);
+}
+END_TEST
+
+START_TEST(test_lcs_is_1_b)
+{
+	ck_assert_int_eq(_lcs_distance("a", "bad"), 1);
+}
+END_TEST
+
+START_TEST(test_lcs_is_1_c)
+{
+	ck_assert_int_eq(_lcs_distance("a", "bade"), 1);
+}
+END_TEST
+
+START_TEST(test_lcs_is_1_d)
+{
+	ck_assert_int_eq(_lcs_distance("a", "badef"), 1);
+}
+END_TEST
+
+START_TEST(test_lcs_is_1_e)
+{
+	ck_assert_int_eq(_lcs_distance("ab", "b"), 1);
+}
+END_TEST
+
+START_TEST(test_lcs_is_1_f)
+{
+	ck_assert_int_eq(_lcs_distance("ab", "ba"), 1);
+}
+END_TEST
+
+START_TEST(test_lcs_is_1_g)
+{
+	ck_assert_int_eq(_lcs_distance("ab", "bad"), 1);
+}
+END_TEST
+
+START_TEST(test_lcs_is_1_h)
+{
+	ck_assert_int_eq(_lcs_distance("ab", "bade"), 1);
+}
+END_TEST
+
+START_TEST(test_lcs_is_1_i)
+{
+	ck_assert_int_eq(_lcs_distance("ab", "badef"), 1);
+}
+END_TEST
+
+START_TEST(test_lcs_is_1_j)
+{
+	ck_assert_int_eq(_lcs_distance("abc", "b"), 1);
+}
+END_TEST
+
+START_TEST(test_lcs_is_1_k)
+{
+	ck_assert_int_eq(_lcs_distance("abc", "ba"), 1);
+}
+END_TEST
+
+START_TEST(test_lcs_is_1_l)
+{
+	ck_assert_int_eq(_lcs_distance("abc", "bad"), 1);
+}
+END_TEST
+
+START_TEST(test_lcs_is_1_m)
+{
+	ck_assert_int_eq(_lcs_distance("abc", "bade"), 1);
+}
+END_TEST
+
+START_TEST(test_lcs_is_1_n)
+{
+	ck_assert_int_eq(_lcs_distance("abc", "badef"), 1);
+}
+END_TEST
+
+START_TEST(test_lcs_is_1_o)
+{
+	ck_assert_int_eq(_lcs_distance("abcd", "b"), 1);
+}
+END_TEST
+
+START_TEST(test_lcs_is_1_p)
+{
+	ck_assert_int_eq(_lcs_distance("abcd", "ba"), 1);
+}
+END_TEST
+
+START_TEST(test_lcs_is_1_q)
+{
+	ck_assert_int_eq(_lcs_distance("abcde", "b"), 1);
+}
+END_TEST
+
+START_TEST(test_lcs_is_1_r)
+{
+	ck_assert_int_eq(_lcs_distance("abcde", "ba"), 1);
+}
+END_TEST
+
+START_TEST(test_lcs_is_1_s)
+{
+	ck_assert_int_eq(_lcs_distance("abcdef", "b"), 1);
+}
+END_TEST
+
+START_TEST(test_lcs_is_1_t)
+{
+	ck_assert_int_eq(_lcs_distance("abcdef", "ba"), 1);
+}
+END_TEST
+
+START_TEST(test_lcs_is_2_a)
+{
+	ck_assert_int_eq(_lcs_distance("abcd", "bad"), 2);
+}
+END_TEST
+
+START_TEST(test_lcs_is_2_b)
+{
+	ck_assert_int_eq(_lcs_distance("abcd", "bade"), 2);
+}
+END_TEST
+
+START_TEST(test_lcs_is_2_c)
+{
+	ck_assert_int_eq(_lcs_distance("abcd", "badef"), 2);
+}
+END_TEST
+
+START_TEST(test_lcs_is_2_d)
+{
+	ck_assert_int_eq(_lcs_distance("abcde", "bad"), 2);
+}
+END_TEST
+
+START_TEST(test_lcs_is_2_e)
+{
+	ck_assert_int_eq(_lcs_distance("abcdef", "bad"), 2);
+}
+END_TEST
+
+START_TEST(test_lcs_is_3_a)
+{
+	ck_assert_int_eq(_lcs_distance("abcde", "bade"), 3);
+}
+END_TEST
+
+START_TEST(test_lcs_is_3_b)
+{
+	ck_assert_int_eq(_lcs_distance("abcde", "badef"), 3);
+}
+END_TEST
+
+START_TEST(test_lcs_is_3_c)
+{
+	ck_assert_int_eq(_lcs_distance("abcdef", "bade"), 3);
+}
+END_TEST
+
+START_TEST(test_lcs_is_4_a)
+{
+	ck_assert_int_eq(_lcs_distance("abcdef", "badef"), 4);
+}
+END_TEST
+
+START_TEST(test_lcs_is_4_b)
+{
+	ck_assert_int_eq(_lcs_distance("ABDCEF", "BADEF"), 4);
+}
+END_TEST
+
 /*
  * Create test suite
  */
@@ -82,6 +268,37 @@ lcs_suite(void)
 	tcase_add_test(tc_core, test_first_empty);
 	tcase_add_test(tc_core, test_second_empty);
 	tcase_add_test(tc_core, test_both_empty);
+	tcase_add_test(tc_core, test_lcs_is_0);
+	tcase_add_test(tc_core, test_lcs_is_1_a);
+	tcase_add_test(tc_core, test_lcs_is_1_b);
+	tcase_add_test(tc_core, test_lcs_is_1_c);
+	tcase_add_test(tc_core, test_lcs_is_1_d);
+	tcase_add_test(tc_core, test_lcs_is_1_e);
+	tcase_add_test(tc_core, test_lcs_is_1_f);
+	tcase_add_test(tc_core, test_lcs_is_1_g);
+	tcase_add_test(tc_core, test_lcs_is_1_h);
+	tcase_add_test(tc_core, test_lcs_is_1_i);
+	tcase_add_test(tc_core, test_lcs_is_1_j);
+	tcase_add_test(tc_core, test_lcs_is_1_k);
+	tcase_add_test(tc_core, test_lcs_is_1_l);
+	tcase_add_test(tc_core, test_lcs_is_1_m);
+	tcase_add_test(tc_core, test_lcs_is_1_n);
+	tcase_add_test(tc_core, test_lcs_is_1_o);
+	tcase_add_test(tc_core, test_lcs_is_1_p);
+	tcase_add_test(tc_core, test_lcs_is_1_q);
+	tcase_add_test(tc_core, test_lcs_is_1_r);
+	tcase_add_test(tc_core, test_lcs_is_1_s);
+	tcase_add_test(tc_core, test_lcs_is_1_t);
+	tcase_add_test(tc_core, test_lcs_is_2_a);
+	tcase_add_test(tc_core, test_lcs_is_2_b);
+	tcase_add_test(tc_core, test_lcs_is_2_c);
+	tcase_add_test(tc_core, test_lcs_is_2_d);
+	tcase_add_test(tc_core, test_lcs_is_2_e);
+	tcase_add_test(tc_core, test_lcs_is_3_a);
+	tcase_add_test(tc_core, test_lcs_is_3_b);
+	tcase_add_test(tc_core, test_lcs_is_3_c);
+	tcase_add_test(tc_core, test_lcs_is_4_a);
+	tcase_add_test(tc_core, test_lcs_is_4_b);
 	suite_add_tcase(s, tc_core);
 
 	return s;
